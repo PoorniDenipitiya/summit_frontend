6297049c8b26c747cbf4542b7fc3df5c5519f9cc
PoorniDenipitiya <denipitiyadpu.21@uom.lk>
2024-11-17 07:05:12 +0530

    update

--- root-config/webpack.config.js
+++ root-config/webpack.config.js
@@ -26,3 +26,33 @@ module.exports = (webpackConfigEnv, argv) => {
     ],
   });
 };
+
+/*
+import { merge } from "webpack-merge";
+import singleSpaDefaults from "webpack-config-single-spa";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default (webpackConfigEnv, argv) => {
+  const orgName = "ecommerce";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+  });
+};
+*/
